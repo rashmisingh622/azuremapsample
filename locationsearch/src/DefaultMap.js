d519893228948eb4c5a33906bc015ce3b3fba6bf
Rashmi Singh <narcissa622@gmail.com>
2023-08-09 10:26:44 -0400

    changes for displaying maps

--- locationsearch/src/DefaultMap.js
+++ locationsearch/src/DefaultMap.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import {AzureMap, AzureMapsProvider, IAzureMapOptions} from 'react-azure-maps';
 import { AuthenticationType } from 'azure-maps-control';
+const MapsSearch = require("@azure-rest/maps-search").default,
+{ isUnexpected } = require("@azure-rest/maps-search");
+const { AzureKeyCredential } = require("@azure/core-auth");
+
+const subscriptionKey = "FaQ8Das_uLbff9_kZOQCz_UXGRxbMVjlgMdhJlVQkS8";
+const credential = new AzureKeyCredential(subscriptionKey);
+const client = MapsSearch(credential);
 
 const option: IAzureMapOptions = {
     authOptions: {
         authType: AuthenticationType.subscriptionKey,
-        subscriptionKey: ""
+        subscriptionKey: "FaQ8Das_uLbff9_kZOQCz_UXGRxbMVjlgMdhJlVQkS8"
     },
 }
+
+const response = await client.path("/search/fuzzy/{format}", "json").get({
+    queryParameters: {
+      query: "Starbucks",
+      lat: 47.61559,
+      lon: -122.33817,
+      countrySet: ["US"],
+    },
+  });
+
+  // Handle the error response
+  if (isUnexpected(response)) {
+    throw response.body.error;
+  }
+  // Log the result
+  console.log(`Starbucks search result nearby Seattle:`);
+  response.body.results.forEach((result) => {
+    console.log(`\
+      * ${result.address.streetNumber} ${result.address.streetName}
+        ${result.address.municipality} ${result.address.countryCode} ${
+      result.address.postalCode
+    }
+        Coordinate: (${result.position.lat.toFixed(4)}, ${result.position.lon.toFixed(4)})\
+    `);
+  });
 
 const DefaultMap: React.FC = () => (
     <div style={{height: '800px'}}>
